dd67bc1e608ee1f124a094fce713338a631026b9
prodbuild <shamajit@gmail.com>
2016-01-05 09:47:03 +0530

    sdgdg

---
+++ Account/Account.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApplication
+{
+    public class Account
+    {
+        public readonly string AccountNumber;
+
+        public Account(string accountNumber)
+        {
+            this.AccountNumber = accountNumber;
+        }
+
+        private int balance;
+        private int minimumBalance = 10000;
+
+        public int Balance
+        {
+            get { return balance; }
+        }
+
+        public int MinimumBalance
+        {
+            get { return minimumBalance; }
+        }
+
+        public void Deposit(int amount)
+        {
+            this.balance += amount;
+        }
+
+        public void Withdraw(int amount)
+        {
+            this.balance -= amount;
+        }
+
+        public void TransferFunds(Account destination, int amount)
+        {
+            if ((this.balance - amount) < this.minimumBalance)
+            { throw new InsufficientFundsException(string.Format("Insufficinet fund in {0}", this.GetType().Name)); }
+
+            destination.Deposit(amount);
+
+            this.Withdraw(amount);
+        }
+
+
+    }
+
+    public class InsufficientFundsException : ApplicationException
+    {
+        public string ErrorMessage { get; private set; }
+
+        public InsufficientFundsException(string message)
+        {
+            this.ErrorMessage = message;
+        }
+    }
+
+}
---
+++ NUnitTest/AccountTest.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using BankApplication;
+using System.Reflection;
+
+
+namespace NUnitTestDemo
+{
+
+    [TestFixture]
+    [Category("Accounting")]
+    public class AccountTest
+    {
+        private Account sourceAccount;
+        private Account destinationAccount;
+
+
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            // == Assemble ==
+            this.sourceAccount = new Account("SB34589");
+            this.sourceAccount.Deposit(40000);
+
+            this.destinationAccount = new Account("SB85736");
+            this.destinationAccount.Deposit(60000);
+        }
+
+
+        [Test]
+        [Category("Deduction")]
+        public void
+            Account_TransferFundFromOneAccountToAnotherAccount_TrasferSuccessfull_SourceAccountContainsMoreMoneyThanDestinationAccount()
+        {
+
+
+            // == Act ==
+            this.sourceAccount.TransferFunds(this.destinationAccount, 20000);
+
+            // == Assert ==
+
+            //Console.WriteLine("In {0} sourceAccountBalance is {1}",MethodBase.GetCurrentMethod().Name, this.sourceAccount.Balance);
+
+
+            Assert.AreEqual(this.sourceAccount.Balance, 38000, "Failed {0} has not the same value", new object[] { "SourceAccount" });
+            Assert.That(this.sourceAccount.Balance, Is.EqualTo(3800), "Failed {0} has not the same value", new object[] { "SourceAccount" });
+
+
+
+            Assert.AreEqual(this.destinationAccount.Balance, 62000);
+
+            Assert.That(this.sourceAccount.Balance, Is.EqualTo(3800), "Failed {0} has not the same value",
+                new object[] { "SourceAccount" });
+
+            Assert.That(this.sourceAccount.Balance, Is.EqualTo(3800).Or.EqualTo(60000), "Failed {0} has not the same value",
+                new object[] { "SourceAccount" });
+
+
+            Assert.Pass("Successfully passed");
+
+            var count = 55;
+            Assert.GreaterOrEqual(count, 50, "The values are not equal");
+            Assert.That(count, Is.GreaterThanOrEqualTo(50), "The values are not equal");
+
+        }
+
+
+        [Test]
+        [Category("FundTransfer")]
+        public void Test_Transfer_Of_Funds_With_Insufficient_Funds_InAccounts(
+            [Values(10000, 2000, 3000)]int amountTobeTransferred)
+        {
+
+            // == Assertion ==
+            Assert.Throws(typeof(InsufficientFundsException), () =>
+            {
+                this.sourceAccount.TransferFunds(this.destinationAccount, amountTobeTransferred);
+            });
+
+            Assert.That(() =>
+            {
+                this.sourceAccount.TransferFunds(this.destinationAccount, amountTobeTransferred);
+            }, Throws.TypeOf<InsufficientFundsException>());
+
+
+            var sourceList = new List<int> { 321, 567, 873 };
+            var targetList = new List<int> { 123, 456, 987 };
+
+            CollectionAssert.IsSubsetOf(targetList, sourceList, "The lists are not equal");
+            Assert.That(targetList, Is.SubsetOf(sourceList), "The lists are not equal");
+
+
+        }
+
+    }
+
+    [SetUpFixture]
+    public class AccountSetUp
+    {
+        [SetUp]
+        public void RunBeforeAnyFixtureExecutes()
+        {
+
+
+
+
+
+        }
+
+        [TearDown]
+        public void RunAfterAnyFixtureExecutes()
+        {
+
+        }
+    }
+}
